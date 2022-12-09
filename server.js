4158b28d15265f586c754e693c00414f3a8b20db
Vinay <vinaysarda2812@gmail.com>
2022-12-09 23:02:58 +0530

    Vercel deploy

--- server.js
+++ server.js
@@ -14,13 +14,11 @@ const path = require('path')
 app.use('/api/user/', userRoute)
 
 // for production level
-if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, "/client/build")));
 
     app.get("*", (req, res)=>{
         res.sendFile(path.join(__dirname, "/client/build", "index.html"));
     });
-}
 
 // app.get('/', (req, res) => res.send('hello'))
 app.listen(port, () => console.log(`Listening ${port}`))
