4cb0c64be84dc57972e99e4ad1d7ecc97a20731c
Vinay <vinaysarda2812@gmail.com>
2022-12-09 22:28:22 +0530

    Vercel deploy

--- server.js
+++ server.js
@@ -16,11 +16,9 @@ app.use('/api/user/', userRoute)
 // for production level
 if(process.env.NODE_ENV === 'production'){
 
-    app.use('/' , express.static('client/build'))
-
+    app.use('/' , express.static(__dirname, "client", "build"))
     app.get('*', (req , res)=>{
-
-        res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
 }
 
