0dc12883d99f6ebe1539dc1b5c13aab379a626a0
Vinay <vinaysarda2812@gmail.com>
2023-01-16 15:33:35 +0530

    Build

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ app.use('/api/user/', userRoute)
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "client", "build"), "index.html");
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-    
-
-// app.get('/', (req, res) => res.send('hello'))
 app.listen(port, () => console.log(`Listening ${port}`))
