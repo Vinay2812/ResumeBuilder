57efc86cc9de3c57610f6e003a908eb6f12380da
Vinay <vinaysarda2812@gmail.com>
2023-01-16 16:16:42 +0530

    Build

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const dotenv = require("dotenv")
 const cors = require("cors");
 dotenv.config();
-app.use(express.json())
 app.use(cors({
     origin: '*'
 }));
 
+app.use(express.json())
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB, {useUnifiedTopology:true, useNewUrlParser:true})
 const connection = mongoose.connection
