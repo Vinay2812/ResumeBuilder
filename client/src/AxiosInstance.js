7d187b74afc189bafcf8264b6dc03ef807b36776
Vinay <vinaysarda2812@gmail.com>
2023-01-16 16:00:50 +0530

    Build

--- client/src/AxiosInstance.js
+++ client/src/AxiosInstance.js
@@ -1,2 +1,2 @@
 import axios from "axios"
-export const API = axios.create({baseURL: "https://resume-builder-by-vinay2812.vercel.app/"})
+export const API = axios.create({baseURL: "https://resume-builder-by-vinay.up.railway.app"})
--- server.js
+++ server.js
@@ -3,27 +3,26 @@ const app = express();
 const dotenv = require("dotenv")
 const cors = require("cors");
 dotenv.config();
-app.use(cors({
-    origin: "*"
-}))
-
-const dbConnect = require('./dbConnect')
 app.use(express.json())
+app.use(cors({
+    origin: '*'
+}));
+require('./dbConnect')
 
 // const port = 5000 for local
 const port = process.env.PORT || 5000;
 
 const userRoute = require('./routes/userRoute')
 
-const path = require('path')
 
 app.use('/api/user/', userRoute)
 
-// for production level
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.listen(port, () => console.log(`Listening ${port}`))
 
+// for production level
+const path = require('path')
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(port, () => console.log(`Listening ${port}`))
